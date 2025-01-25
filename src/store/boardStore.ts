1385710716756b3f7962812166074c9c6ea828f2
aratodaniel <aratodana1@gmail.com>
2025-01-26 00:56:18 +0100

    Add store basics

--- src/store/boardStore.ts
+++ src/store/boardStore.ts
@@ -1,10 +1,41 @@
-import { makeAutoObservable } from "mobx";
+import {makeAutoObservable} from "mobx";
+import Field from "../utils/types/Field.ts";
+import randomInteger from "../utils/functions/randomInteger.ts";
 
 class BoardStore {
+    board: Array< Array<Field> > | null = null;
 
     constructor() {
         makeAutoObservable(this);
     }
+
+    initEmptyBoard (size: number) {
+        const localBoard = [];
+        for (let i = 0; i < size; i++) {
+            const row = [];
+            for (let j = 0; j < size; j++) {
+                row.push(Field.EMPTY);
+            }
+            localBoard.push(row);
+        }
+    }
+
+    addRandomMines (numberOfMines: number) {
+        if (!this.board) {
+            console.warn('Call addRandomMines without board');
+            return;
+        }
+        for (let i = 0; i < numberOfMines; i++) {
+            const x = randomInteger(0, this.board.length)
+            const y = randomInteger(0, this.board.length)
+            this.board[x][y] = Field.MINE;
+        }
+        this.calculateNearFields();
+    }
+    calculateNearFields () {
+        // TODO: Implement this, when board is ready
+    }
+
 }
 
 export const boardStore = new BoardStore();
---
+++ src/utils/types/Field.ts
@@ -0,0 +1,14 @@
+enum Field {
+    EMPTY = 0,
+    EMPTY_1= 1,
+    EMPTY_2= 2,
+    EMPTY_3= 3,
+    EMPTY_4 = 4,
+    EMPTY_5 = 5,
+    EMPTY_6 = 6,
+    EMPTY_7 = 7,
+    EMPTY_8 = 8,
+    MINE = 'x'
+}
+
+export default Field;
