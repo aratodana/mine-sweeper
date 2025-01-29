afd1f9da371d33af11d0fe5ea78505dbdc965993
aratodaniel <aratodana1@gmail.com>
2025-01-29 20:36:31 +0100

    Design card part1

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -12,25 +12,58 @@ import {StyleSheet, View, Text, TouchableOpacity, ImageBackground} from 'react-n
 import {Card} from "../utils/types/Card.ts";
 import {useTranslation} from "react-i18next";
 import {gameStore} from "../store/gameStore.ts";
+import CardLevel from "../utils/enum/CardLevel.ts";
+
+const colorConfig = [
+    {
+        level: CardLevel.COMMON,
+        color: '#ca9060',
+    },
+    {
+        level: CardLevel.RARE,
+        color: '#0073ff',
+    },
+    {
+        level: CardLevel.UNIQUE,
+        color: '#13bd00',
+    },
+    {
+        level: CardLevel.LEGENDARY,
+        color: '#9500ff',
+    },
+    {
+        level: CardLevel.MYTHIC,
+        color: '#d40000',
+    },
+]
 
 const styles = StyleSheet.create({
     card: {
         width: '100%',
         aspectRatio: 0.75,
-        borderRadius: 4,
-        backgroundColor: '#a7a7a7',
+        borderRadius: 10,
         justifyContent: 'space-between',
         overflow: 'hidden',
+        borderStyle: 'solid',
+        borderWidth: 4,
     },
     cardTitle: {
-        fontSize: 18,
+        fontSize: 16,
         padding: 4,
+        backgroundColor: '#171717',
+        textAlign: 'center',
     },
     cardDescription: {
         bottom: 0,
-        backgroundColor: '#d5d5d5',
+        backgroundColor: '#171717',
         padding: 4,
-        flexBasis: "30%"
+        flexBasis: "30%",
+        width: '100%',
+        justifyContent: 'center',
+        alignItems:'center',
+        textAlign: 'center',
+        flexWrap: "wrap"
+
     }
 });
 
@@ -43,6 +76,7 @@ interface Props {
 
 const CardView = ({ card, onPress = () => {}, onLongPress = () => {} }: Props): JSX.Element => {
     const { t } = useTranslation();
+    const dominantColor = colorConfig.find(item => item.level === card.level)?.color || 'white';
 
     return  <TouchableOpacity  onPress={() => {
         if (card.price <= gameStore.coins) {
@@ -52,13 +86,12 @@ const CardView = ({ card, onPress = () => {}, onLongPress = () => {} }: Props): 
         }
         onPress()
     }} onLongPress={onLongPress}>
-        <ImageBackground style={styles.card} source={card.image}>
-            <Text style={styles.cardTitle}>
+        <ImageBackground style={[styles.card, {borderColor: dominantColor} ]} source={card.image}>
+            <Text style={[styles.cardTitle, {color: dominantColor}]}>
                 { t(card.title) }
             </Text>
-            <Text style={styles.cardDescription}>
+            <Text style={[styles.cardDescription, {color: dominantColor}]} numberOfLines={2} ellipsizeMode="tail">
                 { t(card.description) }
-                ({ card.level }  { card.price })
             </Text>
         </ImageBackground>
     </TouchableOpacity>
