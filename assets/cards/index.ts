7542de8cf4ca878d8e52e63f2026fe769a90945d
aratodaniel <aratodana1@gmail.com>
2025-02-27 21:35:22 +0100

    Fix image index

--- assets/cards/index.ts
+++ assets/cards/index.ts
@@ -1,14 +1,24 @@
-const images: Record<string, any> = {
-    "flame-wave": require("./flame-wave.png"),
-    "healing-light": require("./healing-light.png"),
-    "stone-spike": require("./stone-spike.png"),
-    "venom-cloud": require("./venom-cloud.png"),
-    "arcane-shield": require("./arcane-shield.png"),
-    "frost-blast": require("./frost-blast.png"),
-    "thunder-clap": require("./thunder-clap.png"),
+const images = {
     "wind-barrier": require("./wind-barrier.png"),
-    "ethereal-wave": require("./ethereal-wave.png"),
+    "void-nova": require("./void-nova.png"),
+    "venom-cloud": require("./venom-cloud.png"),
+    "thunder-clap": require("./thunder-clap.png"),
+    "storm-burst": require("./storm-burst.png"),
+    "stone-spike": require("./stone-spike.png"),
     "starfall": require("./starfall.png"),
+    "shadow-strike": require("./shadow-strike.png"),
+    "radiant-blast": require("./radiant-blast.png"),
+    "phantom-dagger": require("./phantom-dagger.png"),
+    "mystic-flare": require("./mystic-flare.png"),
+    "light-arrow": require("./light-arrow.png"),
+    "healing-light": require("./healing-light.png"),
+    "frost-blast": require("./frost-blast.png"),
+    "flame-wave": require("./flame-wave.png"),
+    "ethereal-wave": require("./ethereal-wave.png"),
+    "crystal-barrier": require("./crystal-barrier.png"),
+    "celestial-hammer": require("./celestial-hammer.png"),
+    "blazing-sphere": require("./blazing-sphere.png"),
+    "arcane-shield": require("./arcane-shield.png"),
 };
 
 export default images;
